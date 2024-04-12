c0e4023e72a24d09f2778c860b13b38623373762
jo <joachim@getkis.io>
2024-04-12 18:10:40 -0400

    last

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    {/* <Routes /> */}
-  </React.StrictMode>
+    <React.StrictMode>{/* <Routes /> */}</React.StrictMode>
 );
