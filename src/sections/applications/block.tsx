09c80f328bb3b8639af8f23d329829618e65f548
ParnetAnastasia <parnetanastasia@gmail.com>
2024-11-27 16:24:56 +0200

    add data_link to handleGetHandlers

--- src/sections/applications/block.tsx
+++ src/sections/applications/block.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Box } from '@mui/material';
 import { useGetDataHandlersList } from 'src/apis/data-handler';
 import { DataQuery } from 'src/types/queries-interface';
-
+import { useDataLink } from 'src/hooks/use-data-link';
 import { BlockViewByComponentType } from './block-view/constants';
 import cable from './helpers/cable';
 import PageDataInCheck from './helpers/pageDataInCheck';
@@ -12,59 +12,62 @@ interface Props {
 }
 
 export default function Block({ block }: Props) {
-  const { getDataHandlers } = useGetDataHandlersList();
   const {
     data: { queries, queries_dispatch },
   } = block.blocs[0];
-
   const path = window.location.href;
   const slplitPath = path.substring(path.lastIndexOf('/') + 1);
 
   const [toLoad, setToLoad] = useState(true);
-
   const wss = useRef<WebSocket | null>(null);
+
+  const { data_link, data_link_ready } = useDataLink();
+  const { getDataHandlers } = useGetDataHandlersList();
 
   const handleGetHandlers = useCallback(
     async (additionalFilters?: any[]) => {
-      const queriesRequest: DataQuery[] = [];
+      if (data_link_ready) {
+        const queriesRequest: DataQuery[] = [];
 
-      queries.forEach((q: any) => {
-        const dataFilters = () => {
-          if (q.filters) {
-            const filtersJSON = JSON.stringify(q.filters);
-            const d_i = PageDataInCheck(filtersJSON, '');
+        queries.forEach((q: any) => {
+          const dataFilters = () => {
+            if (q.filters) {
+              const filtersJSON = JSON.stringify(q.filters);
+              const d_i = PageDataInCheck(filtersJSON, data_link);
 
-            return JSON.parse(d_i);
-          }
-          return q.filters;
-        };
+              return JSON.parse(d_i);
+            }
+            return q.filters;
+          };
 
-        const filters = dataFilters();
+          const filters = dataFilters();
 
-        const query = {
-          id: q.id,
-          url: q.url,
-          page_id: q.page_id || slplitPath,
-          collection_name: q.collection_name,
-          limit: q.limit,
-          filters: [...filters, ...(additionalFilters || [])],
-          special_filters: q.special_filters || [],
-        };
+          const query = {
+            id: q.id,
+            url: q.url,
+            page_id: q.page_id || slplitPath,
+            collection_name: q.collection_name,
+            limit: q.limit,
+            filters: [...filters, ...(additionalFilters || [])],
+            special_filters: q.special_filters || [],
+          };
 
-        queriesRequest.push(query);
-      });
+          queriesRequest.push(query);
+        });
 
-      setToLoad(false);
+        setToLoad(false);
 
-      if (queriesRequest.length) {
-        const result = await getDataHandlers(queriesRequest);
-        return { queriesRequest, queriesResponse: result?.queries };
+        if (queriesRequest.length) {
+          const result = await getDataHandlers(queriesRequest);
+          return { queriesRequest, queriesResponse: result?.queries };
+        }
+
+        return { queriesRequest: [], queriesResponse: [] };
       }
-
       return { queriesRequest: [], queriesResponse: [] };
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [queries, slplitPath]
+    [data_link, data_link_ready, queries, slplitPath]
   );
 
   useEffect(() => {
