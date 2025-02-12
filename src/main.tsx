793d003c3799a1fafb2f92c5eaaebc35387eb15f
vincent <vincent@getkis.io>
2025-02-12 14:49:34 +0100

    🎨 Improve auto update feature

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import BugsnagPerformance from '@bugsnag/browser-performance';
 import Bugsnag from '@bugsnag/js';
-import BugsnagPluginReact from '@bugsnag/plugin-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -12,8 +11,6 @@ import { SnackbarProvider } from './components/snackbar';
 import { Router } from './routes/Routes';
 import ThemeProvider from './theme';
 import '../muiSetup';
-
-require('update-electron-app')();
 
 Bugsnag.start('ddf870d22820d3119afcd00910c7e288');
 BugsnagPerformance.start({ apiKey: 'ddf870d22820d3119afcd00910c7e288' });
