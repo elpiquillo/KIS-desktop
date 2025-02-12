4c478b3e4dda50ffad0f03d3a43fdb1e7bd5d1ef
vincent <vincent@getkis.io>
2025-02-12 14:49:10 +0100

    ✨ Add closing management

--- electron.cjs
+++ electron.cjs
@@ -1,12 +1,20 @@
-/* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
-const { app, BrowserWindow, protocol, dialog } = require('electron');
-const { autoUpdater } = require('electron-updater');
-
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { app, BrowserWindow } = require('electron');
+const { updateElectronApp } = require('update-electron-app');
 
 let mainWindow;
+let isQuitting = false;
+// Setup auto-updates
+updateElectronApp({
+  updateSource: {
+    provider: 'github',
+    repo: 'KIS-desktop',
+    owner: 'elpiquillo',
+  }
+});
 
-app.whenReady().then(() => {
+const createMainWindow = () => {
   mainWindow = new BrowserWindow({
     icon: path.join(__dirname, 'dist', 'assets', 'AppIcon.icns'),
     width: 1280,
@@ -24,20 +32,19 @@ app.whenReady().then(() => {
   const startURL = `file://${path.resolve(__dirname, 'dist', 'index.html')}`;
   mainWindow.loadURL(startURL);
 
-  // Check for updates
-  autoUpdater.checkForUpdatesAndNotify();
-
   // Handle window close event
   mainWindow.on('close', (event) => {
-    event.preventDefault();
-    if (process.platform === 'darwin') {
+    if (process.platform === 'darwin' && !isQuitting) {
+      event.preventDefault();
       mainWindow.hide();
-    } else {
-      mainWindow.minimize();
     }
   });
+};
 
-  // setupAutoUpdater();
+app.whenReady().then(createMainWindow);
+
+app.on('before-quit', () => {
+  isQuitting = true;
 });
 
 app.on('window-all-closed', () => {
@@ -45,69 +52,55 @@ app.on('window-all-closed', () => {
     app.quit();
   }
 });
-
 app.on('activate', () => {
-  if (mainWindow.isDestroyed()) {
-    mainWindow = new BrowserWindow({
-      icon: path.join(__dirname, 'dist', 'assets', 'AppIcon.icns'),
-      width: 1280,
-      height: 720,
-      webPreferences: {
-        nodeIntegration: true,
-      },
-      titleBarStyle: process.platform !== 'darwin' ? 'default' : 'hidden',
-      trafficLightPosition: {
-        x: 15,
-        y: 13,
-      },
-    });
-    mainWindow.loadURL(startURL);
+  if (mainWindow) {
+    mainWindow.show(); // Réaffiche la fenêtre si elle est cachée
   } else {
-    mainWindow.show();
+    createMainWindow();
   }
 });
 
 /**
  * Function to handle updates with electron-updater
  */
-function setupAutoUpdater() {
-  autoUpdater.autoDownload = true; // Set to true if you want to download updates automatically
+// function setupAutoUpdater() {
+//   autoUpdater.autoDownload = true; // Set to true if you want to download updates automatically
 
-  // Listen for update availability
-  autoUpdater.on('update-available', () => {
-    dialog.showMessageBox({
-      type: 'info',
-      title: 'Update Available',
-      message: 'A new update is available. Downloading now...',
-    });
-  });
+//   // Listen for update availability
+//   autoUpdater.on('update-available', () => {
+//     dialog.showMessageBox({
+//       type: 'info',
+//       title: 'Update Available',
+//       message: 'A new update is available. Downloading now...',
+//     });
+//   });
 
-  // Listen for update download completion
-  autoUpdater.on('update-downloaded', () => {
-    dialog.showMessageBox({
-      type: 'info',
-      title: 'Update Ready',
-      message: 'A new update has been downloaded. Restart the app to apply the update?',
-      buttons: ['Restart', 'Later'],
-    }).then((result) => {
-      if (result.response === 0) {
-        autoUpdater.quitAndInstall(); // Restart app and install update
-      }
-    });
-  });
+//   // Listen for update download completion
+//   autoUpdater.on('update-downloaded', () => {
+//     dialog.showMessageBox({
+//       type: 'info',
+//       title: 'Update Ready',
+//       message: 'A new update has been downloaded. Restart the app to apply the update?',
+//       buttons: ['Restart', 'Later'],
+//     }).then((result) => {
+//       if (result.response === 0) {
+//         autoUpdater.quitAndInstall(); // Restart app and install update
+//       }
+//     });
+//   });
 
-  // Listen for errors
-  autoUpdater.on('error', (error) => {
-    console.error('Update Error:', error);
-    dialog.showMessageBox({
-      type: 'error',
-      title: 'Update Error',
-      message: `Failed to update: ${error.message}`,
-    });
-  });
+//   // Listen for errors
+//   autoUpdater.on('error', (error) => {
+//     console.error('Update Error:', error);
+//     dialog.showMessageBox({
+//       type: 'error',
+//       title: 'Update Error',
+//       message: `Failed to update: ${error.message}`,
+//     });
+//   });
 
-  // Periodically check for updates (every 30 minutes)
-  setInterval(() => {
-    autoUpdater.checkForUpdates();
-  }, 30 * 60 * 1000);
-}
+//   // Periodically check for updates (every 30 minutes)
+//   setInterval(() => {
+//     autoUpdater.checkForUpdates();
+//   }, 30 * 60 * 1000);
+// }
